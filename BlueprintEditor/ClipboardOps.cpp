721fd8dbd8f1e8302019f300d005ae697856ecc4
libyyu <libyyu@qq.com>
2026-03-25 18:00:44 +0800

    fix error in C++17

--- BlueprintEditor/ClipboardOps.cpp
+++ BlueprintEditor/ClipboardOps.cpp
@@ -68,9 +68,9 @@ void BlueprintEditor::CopySelectedNodes()
             cp.name = pin.Name;
             cp.type = pin.Type;
             cp.kind = PinKind::Input;
-            cp.numericValue = pin.BoolValue;
-            cp.numericValue = static_cast<int64_t>(pin.IntValue);
-            cp.numericValue = static_cast<double>(pin.FloatValue);
+            cp.boolValue = pin.BoolValue;
+            cp.intValue = static_cast<int64_t>(pin.IntValue);
+            cp.floatValue = pin.FloatValue;
             cp.stringValue = pin.StringValue;
             cp.objectValue = pin.ObjectValue;
             cp.hiddenWhen = pin.HiddenWhen;
@@ -89,9 +89,9 @@ void BlueprintEditor::CopySelectedNodes()
             cp.name = pin.Name;
             cp.type = pin.Type;
             cp.kind = PinKind::Output;
-            cp.numericValue = pin.BoolValue;
-            cp.numericValue = static_cast<int64_t>(pin.IntValue);
-            cp.numericValue = static_cast<double>(pin.FloatValue);
+            cp.boolValue = pin.BoolValue;
+            cp.intValue = static_cast<int64_t>(pin.IntValue);
+            cp.floatValue = pin.FloatValue;
             cp.stringValue = pin.StringValue;
             cp.objectValue = pin.ObjectValue;
             cp.hiddenWhen = pin.HiddenWhen;
@@ -198,9 +198,9 @@ void BlueprintEditor::PasteNodes(ImVec2 pastePosition)
             int newPinId = GetNextId();
             node.Inputs.emplace_back(newPinId, cp.name.c_str(), cp.type);
             auto& pin = node.Inputs.back();
-            pin.BoolValue  = cp.type == PinDataType::Boolean ? std::get<bool>(cp.numericValue) : false;
-            pin.IntValue   = cp.type == PinDataType::Integer ? static_cast<int>(std::get<int64_t>(cp.numericValue)) : 0;
-            pin.FloatValue = cp.type == PinDataType::Float   ? static_cast<float>(std::get<double>(cp.numericValue)) : 0.0f;
+            pin.BoolValue  = cp.boolValue;
+            pin.IntValue   = static_cast<int>(cp.intValue);
+            pin.FloatValue = cp.floatValue;
             pin.StringValue = cp.stringValue;
             pin.ObjectValue = cp.objectValue;
             pin.HiddenWhen = cp.hiddenWhen;
