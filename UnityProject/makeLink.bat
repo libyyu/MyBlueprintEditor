c1fc19a640f8d42f289a5024cbc46f68e4635330
libyyu <389465209@qq.com>
2026-04-25 21:28:53 +0800

    unity project

---
+++ UnityProject/makeLink.bat
@@ -0,0 +1,5 @@
+mklink /J C:\Workspace\C++\MyBlueprintEditor\UnityProject\Assets\Plugins C:\Workspace\C++\MyBlueprintEditor\Unity\Runtime\Plugins
+mklink /J C:\Workspace\C++\MyBlueprintEditor\UnityProject\Assets\Samples C:\Workspace\C++\MyBlueprintEditor\Unity\Samples
+mklink /J C:\Workspace\C++\MyBlueprintEditor\UnityProject\Assets\Scripts C:\Workspace\C++\MyBlueprintEditor\Unity\Runtime\Scripts
+mklink /J C:\Workspace\C++\MyBlueprintEditor\UnityProject\Assets\Data C:\Workspace\C++\MyBlueprintEditor\data
+pause
