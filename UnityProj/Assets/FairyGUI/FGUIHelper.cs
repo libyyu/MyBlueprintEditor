aae2aafa4e628a351b8d16c2878ecee4a230e462
libyyu <389465209@qq.com>
2026-06-14 17:38:21 +0800

    FairyGUI

---
+++ UnityProj/Assets/FairyGUI/FGUIHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FairyGUI
+{
+    public static class FGUIHelper
+    {
+        public static FairyGUI.GComponent ToCom(FairyGUI.GObject obj)
+        {
+            if (obj == null)
+                return null;
+
+            FairyGUI.GComponent comp = null;
+            if (obj.GetType() == typeof(FairyGUI.GLoader))
+            {
+                FairyGUI.GLoader loader = obj as FairyGUI.GLoader;
+                comp = loader.component;
+            }
+            else if (obj.GetType() == typeof(FairyGUI.GComponent))
+            {
+                comp = obj as FairyGUI.GComponent;
+            }
+            else
+            {
+                comp = obj.asCom;
+            }
+            return comp;
+        }
+
+        public static FairyGUI.GObject Find(FairyGUI.GObject root, string path)
+        {
+            FairyGUI.GComponent comp = ToCom(root);
+            if (comp == null)
+                return null;
+
+            FairyGUI.GObject obj = null;
+            string[] ns = path.Split(new char[] { '/' });
+            foreach (string name in ns)
+            {
+                if (comp == null)
+                    return null;
+
+                obj = comp.GetChild(name);
+                if (obj == null)
+                    return null;
+
+                comp = ToCom(obj);
+            }
+
+            return obj;
+        }
+
+        public static FGUIWindow CreateWindow(string n1, string n2)
+        {
+            return new FGUIWindow(n1, n2);
+        }
+
+        public static FairyGUI.UIPanel CreatePanel(string n1, string n2)
+        {
+            GameObject go = new GameObject(n2 + "@" + n1);
+            FairyGUI.UIPanel ret = go.AddComponent<FairyGUI.UIPanel>();
+            ret.packageName = n1;
+            ret.componentName = n2;
+
+            return ret;
+        }
+    }
+
+}
---
+++ UnityProj/Assets/FairyGUI/FGUIWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FairyGUI
+{
+    public class FGUIWindow : FairyGUI.Window
+    {
+        protected string pkgName;
+        protected string resName;
+        public FGUIWindow(string n1, string n2)
+            : base()
+        {
+            pkgName = n1;
+            resName = n2;
+        }
+
+        protected override void OnInit()
+        {
+            FairyGUI.GComponent panel = FairyGUI.UIPackage.CreateObject(pkgName, resName).asCom;
+            panel.gameObjectName = pkgName;
+            //this.rootContainer.gameObject.transform.SetParent(owner.transform);
+            this.contentPane = panel;
+        }
+
+        public FairyGUI.GObject Find(string name)
+        {
+            return FGUIHelper.Find(this.contentPane, name);
+        }
+
+        public void SetSortingOrder(int value, bool apply)
+        {
+            //this.rootContainer._panelOrder = value;
+            this.sortingOrder = value;
+            if (apply)
+                FairyGUI.Stage.inst.ApplyPanelOrder(this.rootContainer);
+        }
+    }
+}
